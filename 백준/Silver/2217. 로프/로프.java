d61ff617baec152b363e076d5580a3e35004b9d8
kimhaechang <81299056+kimhaechang1@users.noreply.github.com>
2023-10-15 17:03:39 +0900

    [Silver IV] Title: 로프, Time: 1252 ms, Memory: 24924 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.java
+++ 백준/Silver/2217. 로프/로프.java
@@ -14,12 +14,16 @@ public class Main{
 		}
 		Arrays.sort(arr);
 		
-		int idx = 0;		
+		int idx = 0;	
+		
+		// 파라메트릭 서치인줄 알았으나
+		// 그냥 그리디 문제인듯
+		// 특정 로프를 시작점으로 두고 해당 로프번호에서 끝까지 사용했다고 했을 때
+		// 로프에 가해지는 최소한의 하중(w/현재 선택가능한 최대 로프개수)보다 로프중 가장 작은 값이 작다면 
+		// 그것은 무게를 더 늘려봐도 되는것이다.
+		// 왜냐하면 해당 시작점 로프를 못쓰는 순간에 도달하지 못했기 때문이다.
+		// 그 반대의 경우에는 해당 시작점에 대해서 더 고려할 필요가 없기 때문에 다음 시작점(idx)으로 포인터를 옮긴다.
 		while(idx < N) {
-			/*System.out.println("w : "+w);
-			System.out.println((double)(w/(N-idx)));
-			System.out.println(idx +" arr[idx] : "+arr[idx]);
-			System.out.println("-----------");*/
 			if((double)(w/(N-idx)) <= arr[idx]) {
 				w++;
 			}else {

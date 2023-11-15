d6cb1c27628cae164c39707409b5588b2d584b97
kimhaechang <81299056+kimhaechang1@users.noreply.github.com>
2023-11-15 14:47:03 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.45 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -2,54 +2,51 @@ import java.util.*;
 
 class Solution {
    public int[] solution(String[] queries) {
-        PriorityQueue<Integer> lower = new PriorityQueue<>((o1, o2)->{
-            return o1 - o2;
-        });
-        PriorityQueue<Integer> higher = new PriorityQueue<>(Collections.reverseOrder());
-        Queue<Integer> queue = new ArrayDeque<>();
+        TreeMap<Integer, Integer> tmap = new TreeMap<>();
         for(String query : queries){
-            String [] q = query.split(" ");
-            String order = q[0];
-            char where = (q[1].toCharArray())[0];
-            if(order.equals("I")){
-                queue.add(Integer.parseInt(q[1]));
-            }else if(order.equals("D")){
-                
-                if(queue.isEmpty()) continue;
-                if(where == '-'){
-                    while(!queue.isEmpty()){
-                        lower.add(queue.poll());
-                    }
-                    lower.poll();
-                    while(!lower.isEmpty()){
-                        queue.add(lower.poll());
-                    }
-                }else{
-                    while(!queue.isEmpty()){
-                        higher.add(queue.poll());
-                    }
-                    
-                    higher.poll();
-                    
-                    while(!higher.isEmpty()){
-                        queue.add(higher.poll());
-                    }
-                }
-            }
-        }
+	            String [] q = query.split(" ");
+	            String order = q[0];
+	            char where = (q[1].toCharArray())[0];
+	            if(order.equals("I")){
+	            	int key = Integer.parseInt(q[1]);
+	            	Integer cnt = tmap.get(key);
+	                if(cnt == null) {
+	                	tmap.put(key, 1);
+	                }else {
+	                	tmap.put(key, ++cnt);
+	                }
+	                
+	            }else if(order.equals("D")){
+	                
+	                if(tmap.isEmpty()) continue;
+	                if(where == '-'){
+	                    Integer lower = tmap.firstKey();
+	                    Integer cnt = tmap.get(lower);
+	                    if(--cnt == 0) {
+	                    	tmap.remove(lower);
+	                    }else {
+	                    	tmap.put(lower, cnt);
+	                    }
+	                }else{
+	                	Integer higher = tmap.lastKey();
+	                    Integer cnt = tmap.get(higher);
+	                    if(--cnt == 0) {
+	                    	tmap.remove(higher);
+	                    }else {
+	                    	tmap.put(higher, cnt);
+	                    }
+	                }
+	            }
+	        }
         
         int[] answer = new int[2];
-        if(queue.isEmpty()){
+        if(tmap.isEmpty()){
             answer[0] = 0;
             answer[1] = 0;
         }else{
-            while(!queue.isEmpty()){
-                int p = queue.poll();
-                lower.add(p);
-                higher.add(p);
-            }
-            answer[0] = higher.peek();
-            answer[1] = lower.peek();
+            
+            answer[0] = tmap.lastKey();
+            answer[1] = tmap.firstKey();
         }
         return answer;
     }

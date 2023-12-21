7f90ce3334677c2f9afc6fcf113d48393c7dae87
kimhaechang <81299056+kimhaechang1@users.noreply.github.com>
2023-12-22 03:47:05 +0900

    [Gold V] Title: 규칙적인 보스돌이, Time: 264 ms, Memory: 23356 KB -BaekjoonHub

--- 백준/Gold/29792. 규칙적인 보스돌이/규칙적인 보스돌이.java
+++ 백준/Gold/29792. 규칙적인 보스돌이/규칙적인 보스돌이.java
@@ -27,6 +27,14 @@ public class Main{
 		PriorityQueue<Long> pq = new PriorityQueue<>((a,b)->{
 			return Long.compare(b, a);
 		});
+		// 캐릭터 별로 선택하고 보스를 이걸 잡냐 저걸 잡냐에 따라 달라짐
+		// 보스에 선택은 잡을수도 있고 안잡을수도있으니 -> 부분집합
+		// 각 캐릭터별로 15분가량의 시간이 주어져있음
+		// 따라서 15분동안의 dp테이블을 채운 최종 최대이익들 중
+		// 높은 순으로 m개 꺼내면 됨
+		// dp 테이블은 현재 사용가능한 보스와 해당 보스를 잡는 시간대별, 그리고 원소값은 메소임
+		// 결국 특정 보스의 체력을 뛰어넘는 데미지를 넣었을 경우 해당 보스에 대한 보상을 받게됨
+		// 여기서 캐릭터는 자유롭지만 보스는 잡다 딴데 갔다가 돌아오는경우는 없으므로ㄴ
 		for(int i = 1;i<n+1;i++) {
 			long dg = dmg[i];
 			long [][] dp = new long[k+1][901];
@@ -41,11 +49,6 @@ public class Main{
 				}
 			}
 			pq.add(dp[k][900]);
-			/*System.out.println(i + "번째 캐릭터의 보스돌이 dp");
-			for(int j = 1;j<k+1;j++) {
-				System.out.println(Arrays.toString(dp[j]));
-			}
-			System.out.println("========================");*/
 		}
 		long sum = 0;
 		while(m-- >0) {

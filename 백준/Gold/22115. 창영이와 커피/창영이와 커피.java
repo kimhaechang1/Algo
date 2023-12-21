fec6ea795ca8a80c3216596f296968f5e181e68a
kimhaechang <81299056+kimhaechang1@users.noreply.github.com>
2023-12-22 04:21:33 +0900

    [Gold V] Title: 창영이와 커피, Time: 168 ms, Memory: 51504 KB -BaekjoonHub

--- 백준/Gold/22115. 창영이와 커피/창영이와 커피.java
+++ 백준/Gold/22115. 창영이와 커피/창영이와 커피.java
@@ -18,14 +18,14 @@ public class Main{
 		}
 		
 		// 각각의 커피를 마시냐 안마시냐로 나뉨 -> 부분집합
+		// 커피의 종류와 카페인을 축으로 돌면서
+		// 정확하게 해당 카페인을 얻을 수 있는 상황이라면 경우의수는 무조건 1이다.
+		// 그리고 해당 카페인을 얻기위하여 지금 커피로 얻는 카페인과 다른 커피의 카페인을 섞을수도있다.
+		// k 가 0이 주어질수도 있음에 주의하자.
 		Arrays.sort(arr);
 		int [][] dp = new int[n+1][k+1];
 		
-		/*for(int i = 1 ;i<n+1;i++) {
-			for(int j = 1;j<k+1;j++) {
-				dp[i][j] = 101;
-			}
-		}*/
+	
 		for(int i = 1;i<n+1;i++) {
 			dp[i][0] = 101;
 		}
@@ -33,12 +33,7 @@ public class Main{
 			dp[0][i] = 101;
 		}
 		
-		/*for(int i = 0 ;i<n+1;i++) {
-			for(int j = 0;j<k+1;j++) {
-				System.out.print(dp[i][j]+" ");
-			}
-			System.out.println();
-		}*/
+		
 		for(int i=1;i<n+1;i++) {
 			for(int j = 0;j<k+1;j++) {
 				dp[i][j] = dp[i-1][j];

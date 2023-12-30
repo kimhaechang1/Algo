b685e1530a9f1a05926a7fc61a2d97bae1395d20
kimhaechang <81299056+kimhaechang1@users.noreply.github.com>
2023-12-31 00:36:04 +0900

    Update 택배 배달과 수거하기.java

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -2,54 +2,59 @@ import java.util.*;
 import java.io.*;
 
 class Solution {
-    // start : 2023 12 30 8:31
+     // start : 2023 12 30 8:31
     // end  : 2023 12 30
-    public static long solution(int cap, int n, int[] d, int[] p) {
+	public static long solution(int cap, int n, int[] d, int[] p) {
         long answer = 0;
         int dsum = 0;
         int psum = 0;
         int di = n-1;
         int pi = n-1;
-        int start = 0;
+        int start = -1;
         while(true) {
-        	if(di < 0 && pi < 0) break;
-        	if((di > -1 && d[di] > 0) || (pi > -1 && p[pi] > 0)) {
-        		// 수거 or 배달을 해야할 지점을 찾았을 때
-        		// 적어도 그곳에서부터 왕복해야함
-        		start = Math.max(di, pi); 
-        		// 최소한 트럭이 가야할 지점을 찾았다면 
-        		// 내려가면서 몇개를 수거하고 몇개를 배달할지를 처리
-        		while(di > -1) {
-        			if(dsum == cap) break;
-        			if(dsum + d[di] > cap) {
-        				d[di] -= (cap - dsum);
+        	dsum = 0;
+        	psum = 0;
+        	start = -1;
+        	for(int i = di;i>-1;i--) {
+                if(dsum == cap){
+                    di = i;break;  
+                } 
+        		if(d[i] > 0) {
+        			start = Math.max(i, start);
+                    
+        			if(dsum + d[i] <= cap) {
+        				dsum += d[i];
+        				d[i] = 0;
+        			}else {
+        				d[i] -= (cap - dsum);
         				dsum += (cap - dsum);
+        				di = i;
         				break;
-        			}else {
-        				dsum+=d[di];d[di] = 0;di--;
         			}
         		}
-        		while(pi > -1) {
-        			if(psum == cap) break;
-        			if(psum + p[pi] > cap) {
-        				p[pi] -= (cap - psum);
+        	}
+        	for(int i= pi;i>-1;i--) {
+                if(psum == cap) {
+                    pi = i;break;
+                }
+        		if(p[i] > 0) {
+        			start = Math.max(i, start);
+        			if(psum + p[i] <= cap) {
+        				psum += p[i];
+        				p[i] = 0;
+        			}else {
+        				p[i] -= (cap - psum);
         				psum += (cap - psum);
+        				pi = i;
         				break;
-        			}else {
-        				psum+=p[pi];p[pi] = 0;pi--;
         			}
         		}
-        		//System.out.println(Arrays.toString(d));
-        		
-        		if(psum > 0 || dsum > 0) {
-        			//System.out.println((start + 1) * 2);
-        			answer += ((start + 1) * 2);
-        			psum = 0;
-        			dsum = 0;
-        		}
-        	}else {
-        		di--;
-            	pi--;
+        	}
+        	if(dsum > 0 ||  psum > 0) {
+        		answer += (start + 1) * 2;
+        	}
+        	if(start == -1) {
+        		break;
         	}
         }
         

88a31ad06aa957269738b5c823bc7fe9f8de1c6a
kimhaechang <81299056+kimhaechang1@users.noreply.github.com>
2023-12-31 00:28:56 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 11.80 ms, Memory: 103 MB -BaekjoonHub

---
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+     // start : 2023 12 30 8:31
+    // end  : 2023 12 30
+	public static long solution(int cap, int n, int[] d, int[] p) {
+        long answer = 0;
+        int dsum = 0;
+        int psum = 0;
+        int di = n-1;
+        int pi = n-1;
+        int start = -1;
+        while(true) {
+        	dsum = 0;
+        	psum = 0;
+        	start = -1;
+        	for(int i = di;i>-1;i--) {
+        		if(d[i] > 0) {
+        			start = Math.max(i, start);
+        			if(dsum + d[i] <= cap) {
+        				dsum += d[i];
+        				d[i] = 0;
+        			}else {
+        				d[i] -= (cap - dsum);
+        				dsum += (cap - dsum);
+        				di = i;
+        				break;
+        			}
+        		}
+        	}
+        	for(int i= pi;i>-1;i--) {
+        		if(p[i] > 0) {
+        			start = Math.max(i, start);
+        			if(psum + p[i] <= cap) {
+        				psum += p[i];
+        				p[i] = 0;
+        			}else {
+        				p[i] -= (cap - psum);
+        				psum += (cap - psum);
+        				pi = i;
+        				break;
+        			}
+        		}
+        	}
+        	if(start > -1) {
+        		answer += (start + 1) * 2;
+        	}
+        	if(dsum == 0 && psum == 0) {
+        		break;
+        	}
+        }
+        
+        return answer;
+    }
+}

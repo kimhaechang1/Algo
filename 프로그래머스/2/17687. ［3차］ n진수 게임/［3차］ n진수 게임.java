f564e949ada175e831374abc6e20e2ea6a68043b
kimhaechang <khc9812121@gmail.com>
2024-04-10 17:34:34 +0900

    [level 2] Title: [3차] n진수 게임, Time: 7.96 ms, Memory: 69.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -0,0 +1,66 @@
+// start 16:15
+// end 
+import java.util.*;
+
+class Solution {
+    static String [] tenToHex = new String[17];
+    public String solution(int n, int t, int m, int p) {
+        // 첫번째 게임 p, 두번째 게임 q
+        // m = 2이고 p = 1 이면서 n =2 이고 구해야 하는 개수가 4개라면
+        // 게임은 
+        // [0]p -> [1]q -> [1p -> 0q] -> [1p -> 1q] -> [1p -> 0q -> 0p]
+        for(int i = 0;i<7;i++){
+            tenToHex[i+10] = String.valueOf((char)('A'+i));
+        }
+        //System.out.println(Arrays.toString(tenToHex));
+        String answer = "";
+        int cnt = 0;
+        int current = 0;
+        int lenStart = 1;
+        StringBuilder sb = new StringBuilder();
+        while(cnt < t){
+            String result = converter(current, n);
+            //System.out.println("current : "+current+" n : "+n+" = "+result);
+            int totalLen = lenStart + result.length();
+            for(int s = lenStart;s < totalLen;s++){
+                if((s % m) == (p % m)){
+                    sb.append(result.charAt(s-lenStart));
+                    cnt++;
+                    if(cnt == t) break;
+                }
+            }
+            lenStart = totalLen;
+            current++;
+        }
+        answer = sb.toString();
+        return answer;
+    }
+    static String converter(int number, int o){
+        if(number == 0){
+            return "0";
+        }else{
+            Stack<String> stack = new Stack<>();
+            StringBuilder result = new StringBuilder();
+            while(number >= o ){
+                int take = number % o;
+                if(take >= 10){
+                    stack.push(tenToHex[take]);        
+                }else{
+                    stack.push(String.valueOf(take));
+                }
+                number /= o;
+            }
+            if(number >= 10) {
+            	stack.push(tenToHex[number]);
+            }else {
+            	stack.push(String.valueOf(number));
+            }
+            
+            //result.append(number);
+            while(!stack.isEmpty()){
+                result.append(stack.pop());
+            }
+            return result.toString();
+        }
+    }
+}

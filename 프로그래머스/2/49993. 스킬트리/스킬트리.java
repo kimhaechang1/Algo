22d94ad0beb17cbf06580173ca3c58b646630303
kimhaechang <khc9812121@gmail.com>
2024-04-12 21:19:45 +0900

    [level 2] Title: 스킬트리, Time: 0.55 ms, Memory: 81.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -0,0 +1,36 @@
+// start 21:01
+// end
+import java.util.*;
+
+class Solution {
+    public int solution(String skill, String[] skill_trees) {
+        int answer = 0;
+        for(int i = 0;i<skill_trees.length;i++){
+            char [] skillFrags = skill.toCharArray();
+            char [] targets = skill_trees[i].toCharArray();
+            boolean isError = false;
+            int [] board = new int[skill.length()];
+            Arrays.fill(board, 99);
+            int idx = 0;
+            for(int j= 0;j<skillFrags.length;j++){
+                char find = skillFrags[j];  
+                for(int k = 0;k<targets.length;k++){
+                    if(targets[k] == find){
+                        board[j] = k;
+                        break;
+                    }
+                }
+            }
+            System.out.println(Arrays.toString(board));
+            for(int j = 0;j<skill.length()-1;j++){
+                if(board[j] > board[j+1]){
+                    isError = true;
+                    break;
+                }
+            }
+            if(isError) continue;
+            answer++;
+        }
+        return answer;
+    }
+}

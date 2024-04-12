2a0df17e600957c08fdd94f8440dd6294edd7502
kimhaechang <khc9812121@gmail.com>
2024-04-12 12:19:09 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.10 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -0,0 +1,37 @@
+// start 12:02
+// end
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, String[] words) {
+        int[] answer = new int[2];
+        HashSet<String> set = new HashSet<>();
+        int stopIdx = -1;
+        set.add(words[0]);
+        for(int i= 1;i<words.length;i++){
+            if(set.contains(words[i])){
+                stopIdx = i+1;
+                break;
+            }
+            char prev = words[i-1].charAt(words[i-1].length()-1);
+            char next = words[i].charAt(0);
+            if(prev != next){
+                stopIdx = i+1;
+                break;
+            }
+            set.add(words[i]);
+        }
+        if(stopIdx != -1){
+            int stage = stopIdx / n;
+            int turn = stopIdx % n;
+            if(turn == 0){
+                turn = n;
+            }else{
+                stage++;
+            }
+            answer[0] = turn;
+            answer[1] = stage;
+        }
+        return answer;
+    }
+}

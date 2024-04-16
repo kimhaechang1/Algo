99c79693d073c19b3c2416e19bacd813a9448aa4
kimhaechang <khc9812121@gmail.com>
2024-04-16 09:02:51 +0900

    [level 3] Title: 거스름돈, Time: 19.21 ms, Memory: 52.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -0,0 +1,20 @@
+// start 09:00
+// end
+
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[] money) {
+        long [] dp = new long[n+1];
+        dp[0] = 1;
+        for(int m: money){
+            for(int i= 1;i<=n;i++){
+                if(i >= m){
+                    dp[i] = (dp[i] + dp[i-m]) % 1_000_000_007;    
+                }
+            }
+        }
+        int answer = (int)dp[n];
+        return answer;
+    }
+}

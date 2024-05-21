b267c2d46cefc962caa94e2568e54412e3fb1ebd
kimhaechang <khc9812121@gmail.com>
2024-05-21 13:55:30 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 76 ms, Memory: 11464 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -7,10 +7,13 @@ public class Main{
     public static void main(String [] args) throws Exception{
         BufferedReader bf= new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(bf.readLine());
-        // 1씩 차이가 나야한다.
-        // 56565
         // 각 자리수마다 올 수 있는 수는 0~9
         // 하지만 첫째자리수는 1~9이다
+        // 생각해보면 마지막 자리수에 따라 올 수 있는 수가 거의 정해져있다
+        // 2의 경우 이전 자리수가 1이거나 3이어야 한다.
+        // 3의 경우 이전 자리수가 2이거나 4이어야 한다.
+        // 따라서 이전 값을 가지고 쌓아올라가므로 dp 점화식이 가능하다.
+        // 그런데 0의 경우 항상 이전자리가 1일수 밖에 없고, 자리수에 9가 오려면 이전자리수가 8일수 밖에 없다. 이 경우만 예외처리한다.
         dp = new long[10][n+1];
 
         for(int i = 1;i<=9;i++){
@@ -33,10 +36,6 @@ public class Main{
         for(int i= 0;i<=9;i++) sum = ((sum % 1_000_000_000) + (dp[i][n] % 1_000_000_000)) % 1_000_000_000;
         System.out.print(sum);
 
-        // 2 -> 1 또는 3
-        // 3 -> 2 또는 4
-        // 길이가 1인 계단수: 1, 2, 3, ..., 9 -> 9개
-        // 길이가 2인 계단수: 12, 21, 23, ..., 98 -> 17개
-        // 길이가 3인 계단수: 121, 123, 232, 234, 343, 345, 321, 323, 431 232, 234,
+        
     }
 }

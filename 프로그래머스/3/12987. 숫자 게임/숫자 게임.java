3485bc3c6bb20e92103a719d9a57cd3e2c57cef1
kimhaechang <khc9812121@gmail.com>
2024-05-24 09:49:59 +0900

    [level 3] Title: 숫자 게임, Time: 60.31 ms, Memory: 66.4 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -5,13 +5,12 @@ class Solution {
 
     public int solution(int[] A, int[] B) {
         // A가 건넨 카드에서 B가 이길 수 있는 카드가 있는가? 가 중요함
+        // 순서가 정해졌다고 해서 건드리면 위험하다는건 아님
         Arrays.sort(A);
         Arrays.sort(B);
         int answer = 0;
         int p1 = 0;
         int p2 = 0;
-        // 1 3 5 7
-        // 2 2 6 8
         int n = A.length;
         while(p1 < n && p2 < n){
             if(A[p1] < B[p2]){
@@ -21,7 +20,6 @@ class Solution {
             }else{
                 p2++;
             }
-            //System.out.println("p1: "+p1+" p2: "+p2);
         }
         
 

701c43e2ef5f03c58e536b037162df84be5d4e5f
kimhaechang <khc9812121@gmail.com>
2024-06-08 21:17:41 +0900

    [level 2] Title: 숫자 변환하기, Time: 2.58 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    static int [] v;
+    public int solution(int x, int y, int n) {
+        int answer = 0;
+        return bfs(x, y, n);
+    }
+    static int bfs(int x, int y, int n){
+        PriorityQueue<int []> pq =new PriorityQueue<>((a, b)->{
+            return a[1] - b[1];
+        });
+        boolean [] v = new boolean[1000001];
+        pq.add(new int[]{x, 0});
+        while(!pq.isEmpty()){
+            int [] now = pq.poll();
+            if(now[0] == y){
+                return now[1];
+            }
+            if(now[0] + n < v.length && !v[now[0]+n]){
+                v[now[0]+n] = true;
+                pq.add(new int[]{now[0]+n, now[1]+1});
+            }
+            if(now[0] * 2 < v.length && !v[now[0]*2]){
+                v[now[0] * 2] = true;
+                pq.add(new int[]{now[0] * 2, now[1]+1});
+            }
+            if(now[0] * 3 < v.length && !v[now[0]*3]){
+                v[now[0]*3] = true;
+                pq.add(new int[]{now[0] * 3, now[1]+1});
+            }
+        }
+        return -1;
+    }
+}

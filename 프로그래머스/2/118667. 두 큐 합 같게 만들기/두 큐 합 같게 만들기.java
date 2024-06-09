5d6405efcd37b9216e5d52d98b7de8ab0f0d8010
kimhaechang <khc9812121@gmail.com>
2024-06-09 17:49:06 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 26.04 ms, Memory: 99.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] queue1, int[] queue2) {
+        int answer = 0;
+        // 한쪽 큐에서 pop과 동시에 insert를 수행하고 여기까지가 1회
+        // 어떠한 짓을 해도 두 큐의 합이 같아질 수 없으면 -1
+        long sum = 0;
+        long left = 0;
+        long right = 0;
+        Queue<Integer> lq = new ArrayDeque<>();
+        Queue<Integer> rq = new ArrayDeque<>();
+        for(int i = 0;i<queue1.length;i++){
+            sum += queue1[i];
+            left += queue1[i];
+            lq.add(queue1[i]);
+        }
+        for(int i = 0;i<queue2.length;i++){
+            sum += queue2[i];
+            right += queue2[i];
+            rq.add(queue2[i]);
+        }
+        int n = lq.size() + rq.size();
+        long target = sum/2;
+        boolean flag = false;
+        // 최대 반복가능한 횟수를 지정해야 한다.
+        // 왜냐하면 적절치 못한 차이 때문에 좌우 왔다갔다하는 수가 존재할 수 있기 때문
+        while(answer <= 3*n){
+            if(left == target && right == target){
+                flag = true;
+                break;
+            }
+            answer++;
+            if(left > target){
+                int temp = lq.peek();
+                rq.add(lq.poll());
+                left -= temp;
+                right += temp;
+            }else if(right > target){
+                int temp = rq.peek();
+                lq.add(rq.poll());
+                right -= temp;
+                left += temp;
+            }else{
+                return -1;
+            }
+        }
+        return flag ? answer : -1;
+    }
+}

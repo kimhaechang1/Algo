ffe74a6245a63608d712a26cb777f4700db837d9
kimhaechang <khc9812121@gmail.com>
2024-07-28 13:42:58 +0900

    [Silver I] Title: 신입 사원, Time: 812 ms, Memory: 297464 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -4,18 +4,6 @@ import java.io.*;
 public class Main{
     static StringTokenizer stk;
     static int T;
-    static class Data{
-        int s1;
-        int s2;
-
-        public Data(int s1, int s2){
-            this.s1 = s1;
-            this.s2 = s2;
-        }
-        public String toString(){
-            return "[ s1 : "+s1 + " s2: "+s2+ " ]";
-        }
-    }
     static int n;
     public static void main(String[] args) throws Exception{
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -23,38 +11,31 @@ public class Main{
         StringBuilder sb = new StringBuilder();
         while(T-- > 0){
             n = Integer.parseInt(bf.readLine());
-            Data [] drr = new Data[n];
+            int[] rank = new int[n+1];
             for(int i= 0;i<n;i++){
                 stk = new StringTokenizer(bf.readLine());
-                drr[i] = new Data(Integer.parseInt(stk.nextToken()), Integer.parseInt(stk.nextToken()));
+                int r1 = Integer.parseInt(stk.nextToken());
+                int r2 = Integer.parseInt(stk.nextToken());
+                rank[r1] = r2;
+
             }
 
             // 정답을 구하기 위해서는 한쪽만 오름차순이면 반대쪽은 상관없으므로
             // 한쪽 기준으로 오름차순을 해놓자.
             // 한쪽을 기준으로 오름차순, 반대쪽을 기준으로 내림차순 정렬
-            Arrays.sort(drr, (a, b)-> {
-               if(b.s1 == a.s1){
-                   return b.s2 - a.s2;
-               }
-               return a.s1 - b.s1;
-            });
 
-
-            int s2Min = drr[0].s2;
-            for(int i = 1;i<n;i++){
-                if(s2Min > drr[i].s2) {
-                    s2Min = Math.min(s2Min, drr[i].s2);
-                    // 이러면 적어도 하나만을 만족한다. 왜냐하면 s2의 등수가 더 높은 등급이기 때문
-                    continue;
+            // 별도의 풀이 추가
+            // 어짜피 한쪽만 검사할거라면 반대쪽은 계수정렬을 해도 되지않을까
+            // 왜냐하면 절대 겹치는 등수가없다고 했으므로
+            int min = rank[1];
+            int cnt = 1;
+            for(int i = 2;i<n+1;i++){
+                if(min > rank[i]) {
+                    min = rank[i];
+                    cnt++;
                 }
-                drr[i].s2 = -1;
             }
 
-            int cnt = 0;
-            for(int i = 0;i<n;i++){
-                if(drr[i].s2 == -1) continue;
-                cnt++;
-            }
             sb.append(cnt).append("\n");
         }
         System.out.print(sb);

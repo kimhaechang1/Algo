a70354d2d42e38df06757912f21f5ca189cc98b9
kimhaechang <khc9812121@gmail.com>
2024-08-21 00:11:16 +0900

    [level 3] Title: [PCCP 기출문제] 4번 / 수레 움직이기, Time: 4.56 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/3/250134. ［PCCP 기출문제］ 4번 ／ 수레 움직이기/［PCCP 기출문제］ 4번 ／ 수레 움직이기.java
+++ 프로그래머스/3/250134. ［PCCP 기출문제］ 4번 ／ 수레 움직이기/［PCCP 기출문제］ 4번 ／ 수레 움직이기.java
@@ -5,91 +5,91 @@ class Solution {
     static int m;
     static int[] dy = {-1,1,0,0};
     static int[] dx = {0,0,-1,1};
-    static int[] r, b;
+    static int rsy, rsx, rty, rtx, bsy, bsx, bty, btx;
     static int min;
     static int[][] map;
-    static boolean[][] rv;
-    static boolean[][] bv;
+    static boolean[][] rv, bv;
     public int solution(int[][] maze) {
-        int answer = 0;
         n = maze.length;
         m = maze[0].length;
         map = maze;
         // 빨강 수레 하나, 파란 수레 하나.
         // 각 턴마다 모든 수레를 상하좌우 인접한 칸으로 이동시켜야함
         // dfs 로 접근
-        r = new int[2];
-        b = new int[2];
         rv = new boolean[n][m];
         bv = new boolean[n][m];
         for(int i= 0;i<maze.length;i++) {
             for(int j = 0;j<maze[0].length;j++) {
                 if (maze[i][j] == 1) {
-                    r[0] = i;
-                    r[1] = j;
+                    rsy = i;
+                    rsx = j;
                     rv[i][j] = true;
                 } else if (maze[i][j] == 2) {
-                    b[0] = i;
-                    b[1] = j;
+                    bsy = i;
+                    bsx = j;
                     bv[i][j] = true;
+                } else if (maze[i][j] == 3) {
+                    rty = i;
+                    rtx = j;
+                } else if (maze[i][j] == 4) {
+                    bty = i;
+                    btx = j;
                 }
             }
         }
+
         min = Integer.MAX_VALUE;
-        dfs(0, r, b);
+        dfs(rsy, rsx, bsy, bsx, 0);
         return min == Integer.MAX_VALUE ? 0 : min;
     }
-    static void dfs(int depth, int[] r, int[] b) {
+    static void dfs(int ry, int rx, int by, int bx, int turn) {
 
-        if(map[b[0]][b[1]] == 4 && map[r[0]][r[1]] == 3) {
-            min = Math.min(min, depth);
+        if(ry == rty && rx == rtx && by == bty && bx == btx) {
+            min = Math.min(min, turn);
             return;
         }
+        int nry, nrx, nby, nbx;
+        if(map[ry][rx] == 3) {
+            for (int d = 0; d < 4; d++) {
+                nby = by + dy[d];
+                nbx = bx + dx[d];
+                if (OOB(nby, nbx)) continue;
+                if (bv[nby][nbx] || map[nby][nbx] == 5) continue;
+                if (nby == ry && nbx == rx) continue;
+                bv[nby][nbx] = true;
+                dfs(ry, rx, nby, nbx, turn + 1);
+                bv[nby][nbx] = false;
 
-        int ry, rx, by, bx;
-
-        if (map[r[0]][r[1]] == 3) {
-            for(int dir = 0;dir < 4;dir++) {
-                by = b[0] + dy[dir];
-                bx = b[1] + dx[dir];
-                if(OOB(by, bx)) continue;
-                if(bv[by][bx] || map[by][bx] == 5) continue;
-                if(!(by == r[0] && bx == r[1])) {
-                    bv[by][bx] = true;
-                    dfs(depth+1,  r, new int[]{by, bx});
-                    bv[by][bx] = false;
-                }
             }
-        } else if (map[b[0]][b[1]] == 4) {
-            for(int dir = 0;dir < 4;dir++) {
-                ry = r[0] + dy[dir];
-                rx = r[1] + dx[dir];
-                if(OOB(ry, rx)) continue;
-                if(rv[ry][rx] || map[ry][rx] == 5) continue;
-                if(!(ry == b[0] && rx == b[1])) {
-                    rv[ry][rx] = true;
-                    dfs(depth+1, new int[]{ry, rx}, b);
-                    rv[ry][rx] = false;
-                }
+        } else if(map[by][bx] == 4) {
+            for(int dir = 0;dir<4;dir++) {
+                nry = ry + dy[dir];
+                nrx = rx + dx[dir];
+                if(OOB(nry, nrx)) continue;
+                if(rv[nry][nrx] || map[nry][nrx] == 5) continue;
+                if(nry == by && nrx == bx) continue; // 멈춘곳에 들이받았는지
+                rv[nry][nrx] = true;
+                dfs(nry, nrx, by, bx, turn + 1);
+                rv[nry][nrx] = false;
             }
         } else {
-            for(int dir = 0;dir < 4;dir++) {
-                ry = r[0] + dy[dir];
-                rx = r[1] + dx[dir];
-                if(OOB(ry, rx))continue;
-                if(rv[ry][rx] || map[ry][rx] == 5) continue;
-                for(int dir2 = 0;dir2 < 4;dir2++) {
-                    by = b[0] + dy[dir2];
-                    bx = b[1] + dx[dir2];
-                    if(OOB(by, bx)) continue;
-                    if(bv[by][bx] || map[by][bx] == 5) continue;
-                    if(by == r[0] && bx == r[1] && ry == b[0] && rx == b[1]) continue;
-                    if(by == ry && bx == rx) continue;
-                    rv[ry][rx] = true;
-                    bv[by][bx] = true;
-                    dfs(depth+1, new int[]{ry, rx}, new int[]{by, bx});
-                    bv[by][bx] = false;
-                    rv[ry][rx] = false;
+            for(int dir = 0;dir<4;dir++) {
+                nry = ry + dy[dir];
+                nrx = rx + dx[dir];
+                if(OOB(nry, nrx)) continue;
+                if(rv[nry][nrx] || map[nry][nrx] == 5) continue;
+                for (int d = 0; d < 4; d++) {
+                    nby = by + dy[d];
+                    nbx = bx + dx[d];
+                    if (OOB(nby, nbx)) continue;
+                    if (bv[nby][nbx] || map[nby][nbx] == 5) continue;
+                    if (nby == ry && nbx == rx && nry == by && nrx == bx) continue; // 서로 위치교환 했는지
+                    if (nby == nry && nbx == nrx) continue; // 서로 목적지가 같은지
+                    rv[nry][nrx] = true;
+                    bv[nby][nbx] = true;
+                    dfs(nry, nrx, nby, nbx, turn + 1);
+                    bv[nby][nbx] = false;
+                    rv[nry][nrx] = false;
                 }
             }
         }

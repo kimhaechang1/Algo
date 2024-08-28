ca56acb7551a1c4ad0e3624ff03208a8b67a5627
kimhaechang <khc9812121@gmail.com>
2024-08-28 15:46:26 +0900

    [level 2] Title: 광물 캐기, Time: 19.64 ms, Memory: 98.2 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/README.md
+++ 프로그래머스/2/172927. 광물 캐기/README.md
@@ -1,6 +1,6 @@
 # [level 2] 광물 캐기 - 172927 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172927#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172927) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 28일 15:45:52
+2024년 08월 28일 15:46:23
 
 ### 문제 설명
 
--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 class Solution {
     static int[] idxs = {0, 1, 2};
+    static int[] result;
     static int min;
     static HashMap<String, Integer> map;
     static int[][] need;
@@ -11,6 +12,8 @@ class Solution {
         int answer = 0;
         // 착각했다. 이거 모든 곡괭이를 순열로 돌려야한다....
         // 특정 곡괭이 종류를 다쓸때까지가 아닌, 곡괭이 하나를 다쓸때까지다.
+        
+        
         min = Integer.MAX_VALUE;
         need = new int[][]{  
             {1, 1, 1},
@@ -18,67 +21,52 @@ class Solution {
             {25,5, 1}
         };
         total= 0;
+       
         for(int i= 0;i<picks.length;i++) {
             total += picks[i];
+        }
+        result = new int[total];
+        boolean[] v = new boolean[total];
+        arr = new int[total];
+        int aIdx = 0;
+        for(int i= 0;i<picks.length;i++) {
+            int sCnt = picks[i];
+            while(sCnt-- > 0) {
+                arr[aIdx] = i;
+                aIdx++;
+            }
         }
         // System.out.println("총 광물배열 길이: "+minerals.length);
         map = new HashMap<>();
         map.put("diamond", 0);
         map.put("iron", 1);
         map.put("stone", 2);
-        dfs(0, minerals, picks, 0, 0);
+        dfs(0, v, minerals, picks);
         return min == Integer.MAX_VALUE ? 0 : min;
     }
-    static void dfs(int depth, String[] minerals, int[] p, int mIdx, int sum) {
-        // 백트래킹으로 변경
-        if(depth == total || mIdx == minerals.length) {
-            min = Math.min(sum, min);
+    static void dfs(int depth, boolean[] v, String[] minerals, int[] p) {
+        if(depth == total) {
+            int sum = 0;
+            int midx = 0;
+            for(int i = 0;i<total;i++) {
+                int select = arr[i];
+                int cnt = 0;
+                while(cnt < 5 && midx < minerals.length) {
+                    sum += need[select][map.get(minerals[midx])];
+                    cnt++;
+                    midx++;
+                }
+            }
+            min = Math.min(min, sum);
             return;
         }
         
-        if(p[0] > 0) {
-            // 다이아 사용하기
-            p[0]--;
-            depth++;
-            int i = 0;
-            int s = 0;
-            for(i = mIdx;i< mIdx + 5;i++) {
-                if(i == minerals.length) break;
-                s += need[0][map.get(minerals[i])];
-            }
-            dfs(depth, minerals, p, i, sum + s);
-            depth--;
-            p[0]++;
-        }
-        
-        if(p[1] > 0) {
-            // 철 사용하기
-            p[1]--;
-            depth++;
-            int i= 0;
-            int s = 0;
-            for(i = mIdx;i< mIdx + 5;i++) {
-                if(i == minerals.length) break;
-                s += need[1][map.get(minerals[i])];
-            }
-            dfs(depth, minerals, p, i, sum + s);
-            depth--;
-            p[1]++;
-        }
-        
-        if(p[2] > 0) {
-            // 돌 사용하기
-            p[2]--;
-            depth++;
-            int s = 0;
-            int i = 0;
-            for(i = mIdx;i< mIdx + 5;i++) {
-                if(i == minerals.length) break;
-                s += need[2][map.get(minerals[i])];
-            }
-            dfs(depth, minerals, p, i, sum + s);
-            depth--;
-            p[2]++;
+        for(int i =0;i<total;i++) {
+            if (v[i]) continue;
+            v[i] = true;
+            result[depth] = arr[i];
+            dfs(depth + 1, v, minerals, p);
+            v[i] = false;
         }
     }
 }

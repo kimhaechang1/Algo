14538a47487bc1358ab9ad1fc22743cf45eae930
kimhaechang <khc9812121@gmail.com>
2024-08-28 13:55:01 +0900

    [level 2] Title: 문자열 압축, Time: 7.86 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -18,30 +18,30 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         int cnt = 1;
         prev += len;
-        // System.out.println("sub: "+sub);
         for(int i = prev;i<copy.length();i+=len) {
             String other = copy.substring(i, Math.min(i + len, copy.length()));
+            // 인덱스 out of bound 예외 문제처리 Math.min(upper_bound, 원본 길이);
             
             if(other.length() < len) {
                 sb.append(other);
+                // 기준길이 미달이면 그냥 붙인다.
                 continue;
             }
             if(sub.equals(other)) {
                 cnt++;
+                // 이전에 짜른 부분과 같으면 카운팅
             } else {
                 sb.append(cnt == 1 ? "" : cnt).append(sub);
+                // 한개짜리면 개수는 생략한다
                 cnt = 1;
                 sub = new String(other);
-                /*if(other.equals("c")) {
-                    System.out.println("start c: "+i);
-                }*/
-                
+                // 이전에 짜른 부분 교환
             }
         }
         if(cnt > 0) {
             sb.append(cnt == 1 ? "" : cnt).append(sub);
+            // 마지막 짜른게 남아있다면 넣어주기
         }
-        // System.out.println(sb);
         return sb.length();
     }
 }

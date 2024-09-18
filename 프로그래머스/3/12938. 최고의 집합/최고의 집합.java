705d75d4df7d5c5c814ba828e551f0f34b9b512d
kimhaechang <khc9812121@gmail.com>
2024-09-18 16:54:09 +0900

    [level 3] Title: 최고의 집합, Time: 0.34 ms, Memory: 53.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] solution(int n, int s) {
+        int[] answer = {};
+        int mock = n / s;
+        answer = new int[n];
+        if (s < n) return new int[]{-1};
+        if (s % n == 0) {
+            for(int i = 0;i<n;i++) {
+                answer[i] = s / n;
+            }
+        } else {
+            // 33 33 33 35
+            // 1,257,795
+            // 33 33 34 34
+            // 134 / 4 = 33
+            // 1,258,884
+            int namu = s % n;
+            answer = new int[n];
+            for(int i = 0;i<n- (s % n);i++) {
+                answer[i] = (s / n);
+            }
+            for(int i = (n - (s % n)); i< n;i++) {
+                answer[i] = (s / n) + 1;    
+            }
+            
+        }
+        return answer;
+    }
+}

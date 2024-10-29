47d9410fcd20e323d42802c34ed2a45d7c7db381
kimhaechang <khc9812121@gmail.com>
2024-10-29 13:46:08 +0900

    [Gold IV] Title: 겹치는 선분, Time: 3632 ms, Memory: 329184 KB -BaekjoonHub

---
+++ 백준/Gold/1689. 겹치는 선분/겹치는 선분.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main{
+
+    static StringTokenizer stk;
+    static int n;
+    static int[][] info;
+
+    public static void solve() {
+        TreeMap<Integer, Integer> treeMap = new TreeMap<>();
+        for(int i = 0;i < n;i++) {
+            treeMap.put(info[i][0], treeMap.getOrDefault(info[i][0], 0) + 1);
+            treeMap.put(info[i][1], treeMap.getOrDefault(info[i][1], 0) - 1);
+        }
+        int sum = 0;
+        int ans = 0;
+        for(Map.Entry<Integer, Integer> treeMapEntry: treeMap.entrySet()) {
+            sum += treeMapEntry.getValue();
+            ans = Math.max(sum, ans);
+        }
+        System.out.println(ans);
+    }
+    public static void input() throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(bf.readLine());
+        info = new int[n][2];
+        for(int i = 0;i<n;i++) {
+            stk = new StringTokenizer(bf.readLine());
+            info[i][0] = Integer.parseInt(stk.nextToken());
+            info[i][1] = Integer.parseInt(stk.nextToken());
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        input();
+        solve();
+    }
+
+
+}

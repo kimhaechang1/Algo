eb52ccde764cd72027677cc3241db7aa54357396
kimhaechang <khc9812121@gmail.com>
2024-11-06 13:29:59 +0900

    [level 3] Title: 등대, Time: 61.91 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/3/133500. 등대/등대.java
+++ 프로그래머스/3/133500. 등대/등대.java
@@ -1,66 +1,64 @@
-// 한 간선에 양쪽 끝 중 하나라도 적어도 켜져있어야 한다.
-// 2 -> 1
-// 3 -> 1
-// 4 -> 1
-
-// 1 -> 2, 3, 4, 5
-// 5 -> 1, 6, 7, 8
-
-// 6 -> 5
-// 7 -> 5
-// 8 -> 5
-
-// 자신을 기준으로 간선이 하나만 존재하는 리프노드의 경우 언제나 답에서 제외됨
-// 그러면 리프노드로 갔었던 이전 노드는 적어도 불을 켜야함
 import java.util.*;
 
 class Solution {
+    // 등대와 등대 사이를 오가는 뱃길은 n개
+    // 어느 등대에서 출발해도 모든 등대까지 이동가능 -> 즉 어느 노드에서 출발하던 모든 노드에 닿을 수 있음
+    // 하나의 뱃길 양쪽 끝 등대 중 적어도 하나는 켜져 있도록 등대를 켜두어야 함 -> 즉 간선으로 이어진 두 노드중 하나는 불켜진 노드여야 함
+    // 켜야하는 등대개수의 최솟값 -> 즉 최소한 켜두어야하는 등대들
+    
+    // 예를들어 1번 노드의 경우에는 2, 3, 4, 5 넷중에 하나만 켜져도 됨
+    // 그렇지만 자신을 기준으로 간선이 하나만 있는 노드들은 무조건 둘 중 하나는 켜야함
+    // 자신을 기준으로 간선이 하나만 있는 노드들 -> 더이상 뻗어갈 노드가 없는 노드로서 리프노드가 됨
+    // 리프 노드 자체를 불켜게 되면 예제 1번같은 경우에서는 최소를 만족할 수 없음
+    // 즉 리프 노드의 부모노드를 켜면 혹시나 해당 부모노드를 가진 자식노드들이 여럿 있을 수 있다.
+    
+    // 리프노드까지 타고 내려가는 깊이 우선 탐색을 시전하고
+    // 타고 내려갔는데 리프노드면? 이전 노드의 불을 켠다. 
+    // 그리고 깊이우선탐색에서 이전 노드로 되돌아 가면서 갔다온 노드쪽이 켜지지 않았으면 현재 되돌아온 노드는 반드시 켜야한다.
+    
+    // 그래프를 구성하고, 리프노드를 찾아간 다음, 이전노드를 키고 되돌아가면서 자기가 갔었던 노드가 안켜져있으면 되돌아온 노드를 켜야한다.
+    
     static ArrayList<Integer>[] g;
-    static boolean[] v;
     static boolean[] isLight;
+    static boolean[] v;
     public int solution(int n, int[][] lighthouse) {
         int answer = 0;
         
-        g = new ArrayList[n + 1];
-        isLight = new boolean[n + 1];
-        for(int i = 1;i<n+1;i++) {
-            g[i] = new ArrayList<>();
-        }
-        for(int[] lighth: lighthouse) {
-            int nd1 = lighth[0];
-            int nd2 = lighth[1];
-            g[nd1].add(nd2);
-            g[nd2].add(nd1);
-        }
         v = new boolean[n + 1];
+        isLight = new boolean[n + 1];
+        g = new ArrayList[n + 1];
+        for(int i = 1;i<n+1;i++) g[i] = new ArrayList<>();
+        for(int[] house: lighthouse) {
+            g[house[0]].add(house[1]);
+            g[house[1]].add(house[0]);
+        }
         
-        // 2 -> 3 -> 4 -> 1 -> 5 -> 6 -> 7
+        dfs(1, -1);
         
-        dfs(1, 1);
-        int cnt = 0;
-        for(int i = 1;i<n + 1;i++) 
-            cnt += isLight[i] ? 1 : 0;
+        for(int i = 1;i<isLight.length;i++) {
+            answer += (isLight[i] ? 1 : 0);
+        }
         
-        return cnt;
+        return answer;
     }
-    static void dfs(int prev, int pres) {
+    static void dfs(int node, int prev) {
         
         boolean isLeaf = true;
-        for(int next: g[pres]) {
-            if (next == prev) continue;
+        
+        for(int next: g[node]) {
             if (v[next]) continue;
+            if (next == prev) continue;
             isLeaf = false;
             v[next] = true;
-            dfs(pres, next);
+            dfs(next, node);
             v[next] = false;
             if (!isLight[next]) {
-                isLight[pres] = true;
+                isLight[node] = true;
             }
         }
         
         if(isLeaf) {
             isLight[prev] = true;
         }
-    
     }
 }
